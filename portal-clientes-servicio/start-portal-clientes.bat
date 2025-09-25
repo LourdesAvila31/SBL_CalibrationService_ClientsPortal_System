51c28ba264fe511d39e878b954425760c2b485d6
Lourdes Marién Avila López <lourdes.avila@uabc.edu.mx>
2025-09-24 21:49:57 -0700

    Update package.json and add start script for portal-clientes-servicio

---
+++ portal-clientes-servicio/start-portal-clientes.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ========================================
+echo  Portal Clientes y Servicios ISO 17025
+echo  Iniciando en http://localhost:8001
+echo ========================================
+cd /d "C:\xampp\htdocs\SBL_SISTEMA_SERVICIOS_CLIENTES\portal-clientes-servicio"
+npm run serve
